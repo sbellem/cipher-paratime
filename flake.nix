f0bec3bb9fdc25737d7c6b2bc0a7f746f2c8bfe1
Sylvain Bellemare <sbellem@gmail.com>
2022-03-01 21:34:08 -0600

    flake

--- flake.nix
+++ flake.nix
@@ -5,9 +5,12 @@
     nixpkgs.url      = "github:nixos/nixpkgs/9a3cfff2f9035ca0bd54ff13b608c4492eca6be3";
     rust-overlay.url = "github:oxalica/rust-overlay";
     flake-utils.url  = "github:numtide/flake-utils";
+    fortanix-sgx-tools.url = "github:initc3/nix-fortanix?dir=fortanix-sgx-tools";
+    sgxs-tools.url = "github:initc3/nix-fortanix?dir=sgxs-tools";
+    oasis-core-tools.url = "github:sbellem/oasis-core/142c5daf74fecc5533fc50589b06b3117a509cb0";
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, fortanix-sgx-tools, oasis-core-tools, sgxs-tools }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         overlays = [ (import rust-overlay) ];
@@ -17,6 +20,56 @@
       in
       with pkgs;
       {
+        defaultPackage = rustPlatform.buildRustPackage rec {
+          pname = "cipher-paratime";
+          version = "2.0.0-alpha3";
+
+          src = builtins.path { path = ./.; name = "${pname}-${version}"; };
+
+          cargoSha256 = "sha256-a6tMY2NLeIcMeU2bw9qCtgxD8odlpajzl9N28yZ9n38=";
+
+          LIBCLANG_PATH = "${pkgs.llvmPackages_11.libclang.lib}/lib";
+          
+          rust_toolchain = rust-bin.fromRustupToolchainFile ./rust-toolchain;
+
+          nativeBuildInputs = [
+            oasis-core-tools.defaultPackage.${system}
+            fortanix-sgx-tools.defaultPackage.${system}
+            #sgxs-tools.defaultPackage.${system}
+            clang_11
+            #gcc
+            #gcc_multi
+            llvmPackages_11.libclang.lib
+            #pkg-config
+            (rust_toolchain.override {
+              targets = [ "x86_64-fortanix-unknown-sgx" ];
+            })
+          ];
+
+          #cargoBuildFlags = [ "--package oasis-core-tools" ];
+          #cargoTestFlags = [ "--package oasis-core-tools" ];
+          buildPhase = ''
+            runHook preBuild
+
+            cargo build --release --target x86_64-fortanix-unknown-sgx
+
+            runHook postBuild
+          '';
+          
+          postBuild = ''
+            cargo elf2sgxs --release
+          '';
+
+          installPhase = ''
+            runHook preInstall
+
+            mkdir -p $out/bin
+            cp -r target/x86_64-fortanix-unknown-sgx $out/bin/x86_64-fortanix-unknown-sgx
+
+            runHook postInstall
+            '';
+        };
+
         devShell = mkShell {
           buildInputs = [
             bubblewrap
@@ -28,7 +81,7 @@
             libseccomp
             llvmPackages_11.libclang.lib
             openssl
-            pkgconfig
+            pkg-config
             protobuf
             (rust-bin.nightly."2021-11-04".default.override {
               #extensions = [ "rust-src" "rustfmt" "rust-std" ];
